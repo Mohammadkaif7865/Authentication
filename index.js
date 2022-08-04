7c13c028d5a898ef7e6351786ef9dd917b19cd1b
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-04 19:49:13 +0530

    yes me

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 4000;
 const cors = require('cors');
 app.use(cors());
 const AuthController = require('./controller/authController');
+app.get("/", (req, res) => {
+    res.send("Express server is running");
+  });
 app.use("/api/auth", AuthController);
 // # Now the route will be like /api/auth/authorization_controller_route
 // ! mean ahead logic is handed to auth controller
