624580bab30f3a7d7c2716568d9c2033a614a2b9
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-04 13:02:47 +0530

    yes me

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(cors());
 const AuthController = require('./controller/authController');
 app.use("/api/auth", AuthController);
 // # Now the route will be like /api/auth/authorization_controller_route
+// ! mean ahead logic is handed to auth controller
 app.listen(port, () => {
     console.log(`listing to port : ${port}`);
 });
